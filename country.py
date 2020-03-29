9e33d8b74a8342cd3e736ad69f00ba9b3852cb97
Shashank <esss.25@gmail.com>
2020-03-30 02:00:21 +0530

    change

--- country.py
+++ country.py
@@ -7,7 +7,7 @@ def countrycheck(country):
     for i in range(0,183):
         if country.lower()==api[i]["country"].lower():
             result="Country: "+str(api[i]["country"])+" "
-            result+="Confirmed Cases: "+str(api[i]["cases"])+" "+"Cases Today: "+str(api[i]["todayCases"])+" Deaths: "+str(api[i]["deaths"])+" Recovered: "+str(api[i]["recovered"])
+            result+="Confirmed Cases: "+str(api[i]["cases"])+" Deaths: "+str(api[i]["deaths"])+" Recovered: "+str(api[i]["recovered"])
     if result=="":
         result="not"
     return str(result)
