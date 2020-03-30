e99c11ff40c50a0b4f2e672fb299ad0a29a95cd3
Shashank <esss.25@gmail.com>
2020-03-30 19:26:56 +0530

    change in website

--- testing.py
+++ testing.py
@@ -12,18 +12,20 @@ def corona(state):
     all_rows = soup.find_all('tr')
     for row in all_rows:
         stat = extract_contents(row.find_all('td'))
+        
         if stat:
             if len(stat) == 5:
                 stat = ['', *stat]
                 stats.append(stat)
             elif len(stat) == 6:
                 stats.append(stat)
-    stats[-1][1] = "Total Cases"
-    stats.remove(stats[-1])
     flag=1
     for row in stats:
+        row.remove(row[0])
+    #print(stats)
+    for row in stats:
         if row[1].lower()==state.lower():
-            result="State:{} Indians-Confirmed:{} Foreigners-Confirmed:{} Cured:{} Deaths:{}".format(row[1],row[2],row[3],row[4],row[5])
+            result="State:{} Confirmed Cases:{} Cured:{} Deaths:{}".format(row[1],row[2],row[3],row[4])
             flag=0
         if flag==1:
             result="not matching"
