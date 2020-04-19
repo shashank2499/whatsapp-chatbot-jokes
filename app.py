4d428db1c93ab59cec1293a41d27f20fbbaa50f2
shashank2499 <ersss.25@gmail.com>
2020-04-19 14:11:51 +0530

    Change

--- app.py
+++ app.py
@@ -1,31 +1,27 @@
-from testing import corona
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+
 from utils import fetch_reply
-from country import countrycheck
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
 @app.route("/sms", methods=['POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
     msg = request.form.get('Body')
-    phone_no=request.form.get('From')
-    reply=corona(msg)
-    if reply=="not matching":
-        reply=countrycheck(msg)
-    if reply=="not":
-        reply=fetch_reply(msg,phone_no)
-    if msg.lower()=="no":
-        reply="Please enter the name of the state or country you want to check."
+    phone_no = request.form.get('From')
+    reply = fetch_reply(msg, phone_no)
 
     # Create reply
     resp = MessagingResponse()
     resp.message(reply)
+
     return str(resp)
+
 if __name__ == "__main__":
     app.run(debug=True)
